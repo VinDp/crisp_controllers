2f9a62d0bbc12d2f2a77fa9131197c2d4cde5865
Vincenzo Di Pentima <DiPentima@fzi.de>
2026-02-20 17:06:16 +0100

    Removed ft measuring for debugging

--- src/cartesian_controller.cpp
+++ src/cartesian_controller.cpp
@@ -185,7 +185,7 @@ CartesianController::update(const rclcpp::Time &time,
   // Compute force in endeffector frame
   Eigen::Matrix<double,6,1> F_ee =
     T_ee_ft.toActionMatrix().transpose() * wrench_ext;
-  Eigen::Matrix<double,6,1> F_error = F_des - F_ee;
+  Eigen::Matrix<double,6,1> F_error = F_des ; //- F_ee;
 
   ddx_adm = M_adm.ldlt().solve(F_error - D_adm * dx_adm - K_adm * x_adm);
   // ddx_adm = Md.inverse() * (F_error - Dd * dx_adm - Kd * x_adm);
